eb84d114300ee12a32c7ce69fce54206948f5bec
Landon7676 <landonj@umich.edu>
2025-05-11 21:47:37 -0400

    Added Firebase

---
+++ components/ui/navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white shadow p-4 flex justify-between">
+      <div className="font-bold text-xl">CharitySite</div>
+      <div className="space-x-4">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/donate">Donate</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
+}
