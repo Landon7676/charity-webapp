d5e11ad7a66062b066f45a9e6d601e73c6338f86
kuehnjt <kuehnjt@umich.edu>
2025-05-16 12:44:36 -0400

    color fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./pages/**/*.{ts,tsx}",
-      "./components/**/*.{ts,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          indigo: "#332288",
-          cyan: "#88CCEE",
-          teal: "#44AA99",
-          green: "#117733",
-          olive: "#999933",
-          sand: "#DDCC77",
-          rose: "#CC6677",
-          wine: "#882255",
-          purple: "#AA4499",
-          palegray: "#DDDDDD",
-        },
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        indigo: "#332288",
+        cyan: "#88CCEE",
+        teal: "#44AA99",
+        green: "#117733",
+        olive: "#999933",
+        sand: "#DDCC77",
+        rose: "#CC6677",
+        wine: "#882255",
+        purple: "#AA4499",
+        palegray: "#DDDDDD",
       },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+};
