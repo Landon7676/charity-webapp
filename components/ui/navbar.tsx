1b6a9e08cf28a85c06c4228f0adda9b399b4019b
Landon7676 <landonj@umich.edu>
2025-05-17 00:43:58 -0400

    Name Navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
 
  return (
    <nav className="bg-white shadow p-4 flex justify-between items-center">
-     <div className="font-bold text-xl">CharitySite</div>
+     <div className="font-bold text-xl">GoodFellows</div>
      <div className="space-x-4">
        <Link href="/">Home</Link>
        <Link href="/about">About</Link>
